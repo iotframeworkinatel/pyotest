0b68f37ebcf5ce2c686b617814d45e1fe6501159
Airton Carvalho <airtoncarvalho@wine.com.br>
2025-04-14 10:27:52 -0300

    change language - english

--- __main__.py
+++ __main__.py
@@ -1,21 +1,20 @@
 from nmap_scanner import nmap_scanner
 from scapy_scanner import scapy_explore
 
-# definindo argumentos de linha de comando
 import argparse
 import logging
 from datetime import datetime
 
-parser = argparse.ArgumentParser(description="Scanner de rede para dispositivos IoT")
-parser.add_argument("-v", "--verbose", action="store_true", help="Modo verboso")
-parser.add_argument("-a", "--all", action="store_true", help="Executa todos os métodos de escaneamento")
-parser.add_argument("-i", "--interface", type=str, default="eth0", help="Interface de rede a ser usada (padrão: eth0)")
-parser.add_argument("-ip", "--ip", type=str, help="IP a ser escaneado")
-parser.add_argument("-m", "--mac", type=str, help="MAC a ser escaneado")
-parser.add_argument("-r", "--rede", type=str, default="192.168.0.0/24", help="Rede a ser escaneada (padrão: 192.168.0.0/24)")
-#lista dos scans para executar
-parser.add_argument("-s", "--scans", type=str, default="", help="Métodos de escaneamento a serem executados (nmap, scapy)")
-parser.add_argument("-o", "--output", type=str, default="relatorio.txt", help="Arquivo de saída para o relatório (padrão: relatorio.txt)")
+parser = argparse.ArgumentParser(description="Network scanner for IoT devices")
+parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode")
+parser.add_argument("-a", "--all", action="store_true", help="Run all available scanning methods")
+parser.add_argument("-i", "--interface", type=str, default="eth0", help="Network interface to use (default: eth0)")
+parser.add_argument("-ip", "--ip", type=str, help="IP address to scan")
+parser.add_argument("-m", "--mac", type=str, help="MAC address to scan")
+parser.add_argument("-r", "--network", type=str, default="192.168.0.0/24", help="Network to scan (default: 192.168.0.0/24)")
+parser.add_argument("-s", "--scans", type=str, default="", help="Comma-separated scanning methods to run (nmap, scapy)")
+parser.add_argument("-o", "--output", type=str, default="report.txt", help="Output file name (default: report.txt)")
+
 args = parser.parse_args()
 
 if args.verbose:
@@ -24,16 +23,16 @@ else:
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if args.all:
-    logging.info(f"Escaneando a rede {args.rede} com todos os métodos...")
-    nmap_scanner(args.rede, args.output)
-    scapy_explore(args.rede, args.output)
+    logging.info(f"Scanning network {args.network} with all methods...")
+    nmap_scanner(args.network, args.output)
+    scapy_explore(args.network, args.output)
 
 if "nmap" in args.scans:
-    logging.info("Executando escaneamento Nmap...")
-    nmap_scanner(args.rede, args.output)
-    logging.info("Escaneamento Nmap concluído.")
+    logging.info("Running Nmap scan...")
+    nmap_scanner(args.network, args.output)
+    logging.info("Nmap scan completed.")
 
 if "scapy" in args.scans:
-    logging.info("Executando escaneamento Scapy...")
-    scapy_explore(args.rede, args.output)
-    logging.info("Escaneamento Scapy concluído.")
+    logging.info("Running Scapy scan...")
+    scapy_explore(args.network, args.output)
+    logging.info("Scapy scan completed.")
