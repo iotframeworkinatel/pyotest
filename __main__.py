426deb6d54c1ba4b1436ef52cd2f1b8187e7edc2
Airton Carvalho <airtoncarvalho@wine.com.br>
2025-04-15 17:23:52 -0300

    add extra ports argument

--- __main__.py
+++ __main__.py
@@ -1,9 +1,8 @@
-from nmap_scanner import nmap_scanner
-from scapy_scanner import scapy_explore
+from scanners.nmap_scanner import explore as nmap_explore
+from scanners.scapy_scanner import explore as scapy_explore
 
 import argparse
 import logging
-from datetime import datetime
 
 parser = argparse.ArgumentParser(description="Network scanner for IoT devices")
 parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode")
@@ -11,9 +10,10 @@ parser.add_argument("-a", "--all", action="store_true", help="Run all available 
 parser.add_argument("-i", "--interface", type=str, default="eth0", help="Network interface to use (default: eth0)")
 parser.add_argument("-ip", "--ip", type=str, help="IP address to scan")
 parser.add_argument("-m", "--mac", type=str, help="MAC address to scan")
-parser.add_argument("-r", "--network", type=str, default="192.168.0.0/24", help="Network to scan (default: 192.168.0.0/24)")
+parser.add_argument("-r", "--network", default="auto",type=str, help="Network to scan (default: detected /24 network)")
 parser.add_argument("-s", "--scans", type=str, default="", help="Comma-separated scanning methods to run (nmap, scapy)")
 parser.add_argument("-o", "--output", type=str, default="report.txt", help="Output file name (default: report.txt)")
+parser.add_argument("-p", "--ports", type=str, help="Extra ports to scan (comma-separated)")
 
 args = parser.parse_args()
 
@@ -22,17 +22,12 @@ if args.verbose:
 else:
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-if args.all:
-    logging.info(f"Scanning network {args.network} with all methods...")
-    nmap_scanner(args.network, args.output)
-    scapy_explore(args.network, args.output)
-
-if "nmap" in args.scans:
+if "nmap" in args.scans or args.all:
     logging.info("Running Nmap scan...")
-    nmap_scanner(args.network, args.output)
+    nmap_explore(args)
     logging.info("Nmap scan completed.")
 
-if "scapy" in args.scans:
+if "scapy" in args.scans or args.all:
     logging.info("Running Scapy scan...")
-    scapy_explore(args.network, args.output)
+    scapy_explore(args)
     logging.info("Scapy scan completed.")
