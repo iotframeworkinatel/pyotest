8968b3e83e101fcb1b34c75596f44d28f6af0b58
lrgsouza <lucas_souza@outlook.com>
2025-05-26 09:17:20 -0300

    remove pytest old modules

--- pyotest/test_cases/test_api.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import requests
-
-@pytest.mark.iot_security
-def test_insecure_api():
-    response = requests.get("iot_device_1/api")
-    assert response.status_code == 401, "API acessível sem autenticação!"
