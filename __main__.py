31e4be1ec472bc902fe59284d8c3c918a02934f9
lrgsouza <lucas_souza@outlook.com>
2025-05-26 16:09:07 -0300

    clean main comments

--- __main__.py
+++ __main__.py
@@ -12,9 +12,9 @@ from reports.objects import Report
 parser = argparse.ArgumentParser(description="Network scanner for IoT devices")
 parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode")
 parser.add_argument("-n", "--network", type=str, default="192.168.0.0/27", help="Network to scan (e.g., 192.168.15.0/24)")
-parser.add_argument("-o", "--output", type=str, help="Output file format (e.g., html, json, csv)") ## html ou json
-parser.add_argument("-p", "--ports", type=str, help="Extra ports to scan (comma-separated e.g., 80,443)") ## implementar portas extras para teste
-parser.add_argument("-t", "--test", action="store_true", help="Run vulnerability tests on discovered devices") ## funcionando
+parser.add_argument("-o", "--output", type=str, help="Output file format (e.g., html, json, csv)") 
+parser.add_argument("-p", "--ports", type=str, help="Extra ports to scan (comma-separated e.g., 80,443)") 
+parser.add_argument("-t", "--test", action="store_true", help="Run vulnerability tests on discovered devices") 
 
 args = parser.parse_args()
 
